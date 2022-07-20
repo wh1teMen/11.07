50772e81acd283a46da1c0be7a285b89cfaeeb8b
Belo_cg86@itstepru.onmicrosoft.com <Belo_cg86@itstepru.onmicrosoft.com>
2022-07-20 14:17:13 +0500

    хз

--- 1/Project1/Project1/Source.cpp
+++ 1/Project1/Project1/Source.cpp
@@ -34,66 +34,37 @@ private:
 	int weight;
 	int price;
 };
-int sh(vector<int>& other) {
-	int a = 0;
-	for (auto& el : other) {
-		a += el;
+
+
+
+
+class CarCollection {
+public:
+
+	virtual ~CarCollection() = default;
+
+	void insert(Car* car) {
+		data_.push_back(car);
+		
 	}
-	return a;
-}
-//class big_car :public Car {
-//public:
-//	void a(vector<m_car>&other) {
-//		vector<big_car>q;
-//		q.emplace_back(other);
-//		for (auto el : q) {
-//			cout << el.get_price();
-//			cout << el.get_weight();
-//
-//		}
-//	}
-//};
-//m_car q(m_car& other) {
-//	
-//	static int p, w = 0;
-//	p += other.get_price();
-//	w += other.get_weight();
-//}
+
+	
+	
+	
+private:
+	std::vector<Car*> data_;
+};
 
 
 int main() {
 	system("chcp 1251>nul");
-	m_car car(2500, 2000);
-	m_car car2(4000, 1204);
-	m_car car3(5943, 4943);
+	m_car car1{ 100,3400 };
+	m_car car2{ 102,14400 };
+	CarCollection big;
+	big.insert(&car1); 
+	big.insert(&car2);
 	
-	//cout << car.get_weight()<<"\n";
-	//cout << car.get_price();
-	vector<int>all_price;
-	vector<int>all_weight;
-	all_price.reserve(4);
-	all_price.emplace_back(car.get_price());
-	all_price.emplace_back(car2.get_price());
-	all_price.emplace_back(car3.get_price());
-	all_weight.emplace_back(car.get_weight());
-	all_weight.emplace_back(car2.get_weight());
-	all_weight.emplace_back(car3.get_weight());
-
-	int p_all = 0;
-	int w_all = 0;
-	for (auto el:all_price) {
-		p_all += el;
-		
-	}
-	for (auto el : all_weight) {
-		w_all += el;
-	}
-	cout <<"общая стоимость всех машине: "<<sh(all_price) << endl;
-	cout << "общий вес машин: " << w_all << endl;
-	//a(car);
-
-
-
+	
 
 
 	system("pause>nul");
---
+++ от влада/Example.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+
+class Car {
+public:
+    virtual void bar() = 0;
+};
+
+class ExactCar:public Car {
+public:
+    void bar() {
+    }
+};
+
+class CarCollection {
+public:
+
+    virtual ~CarCollection() = default;
+    virtual ~CarCollection() {
+        for (auto&& car : data_) {
+            delete car;
+        }
+    }
+
+
+    void insert(Car* car) {
+        data_.push_back(car);
+    }
+
+    (Car*&) operator[](int i) {
+        return data_[i];
+    }
+
+private:
+    std::vector<Car*> data_;
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
